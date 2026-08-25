3b1b8ffad2fb1130767279c72c3cfa22d23f5110
チャーキー <chiamiya@outlook.com>
2026-08-25 09:05:22 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+ctx.fillStyle = "#050510";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "#00ffff";
+ctx.fillRect(100, 400, 40, 40);
